00a6197bed22cbee04f99b9cb15368f3ca02eca2
Marina Evers <marina.evers@uni-muenster.de>
2024-06-26 13:34:28 +0200

    get some shading for crossings

--- src/mapping/rendering.py
+++ src/mapping/rendering.py
@@ -189,17 +189,40 @@ def computeValley(img, labelX, labelY, i, j, h, bw):
         d = 2 * h / (bw * bw) * (bw - y2)
     return a, b, c, d
 
-def clostestSegment(img, labelX, labelY, i, j):
+def clostestSegment(img, labelX, labelY, i, j, h, bw):
     y1 = np.where(labelX[i] == labelX[i, j])[0][0]
     x1 = np.where(labelY[:, j] == labelY[i, j])[0][0]
     y2 = np.where(labelX[i] == labelX[i, j])[0][-1]
     x2 = np.where(labelY[:, j] == labelY[i, j])[0][-1]
     if min(i-x1, x2-i) < min(j-y1, y2-j):
-        return img[x1-1, j], False
+        if i - x1 < x2 - i:  # 0.5*(y1+y2)-j < bw:#j-y1 < bw:
+            if j - y1 < y2 - j and j - y1 - (i - x1) < bw:
+                return img[x1-1, j], h / (bw * bw), 0, -2 * h / (bw * bw) * (-bw + x1 + j - y1), 0
+            if y2 - j - (i - x1) < bw:
+                return img[x1-1, j], h / (bw * bw), 0, -2 * h / (bw * bw) * (-bw + x1 + y2 - j), 0
+        else:
+            if j - y1 < y2 - j and j - y1 - (x2 - i) < bw:
+                return img[x1-1, j], h / (bw * bw), 0, -2 * h / (bw * bw) * (-bw + x2 + y2 - j), 0
+            if y2 - j - (x2 - i) < bw:
+                return img[x1-1, j], h / (bw * bw), 0, -2 * h / (bw * bw) * (-bw + x2 + j - y1), 0
+        return img[x1-1, j], 0,0,0,0
     else:
+        # y direction opening
         if min(i-x1, x2-i) == min(j-y1, y2-j):
-            return img[i, y1-1], True
-        return img[i, y1 - 1], False
+            return img[i, y1-1], 0, 0, 0.3, 0.3
+            #return 0, True
+        else:
+            if j-y1 < y2-j:#0.5*(y1+y2)-j < bw:#j-y1 < bw:
+                if i-x1 < x2-i and i-x1-(j-y1) < bw:
+                    return img[i, y1-1], 0, h / (bw * bw), 0, -2 * h / (bw * bw) * (-bw + y1 + i-x1)
+                if x2-i-(j-y1) < bw:
+                    return img[i, y1-1], 0, h / (bw * bw), 0, -2 * h / (bw * bw) * (-bw + y1 + x2-i)
+            else:
+                if i-x1 < x2-i and i-x1-(y2-j) < bw:
+                    return img[i, y1-1], 0, h / (bw * bw), 0, -2 * h / (bw * bw) * (-bw + y2 + x2-i)
+                if x2-i-(y2-j) < bw:
+                    return img[i, y1-1], 0, h / (bw * bw), 0, -2 * h / (bw * bw) * (-bw + y2 + i-x1)
+        return img[i, y1 - 1], 0,0,0,0
 
 
 def render(img, Ia = 40, Is = 215, L=None, h = 0.1, bw = 3, colorCoding=None):
@@ -231,10 +254,10 @@ def render(img, Ia = 40, Is = 215, L=None, h = 0.1, bw = 3, colorCoding=None):
             if img[i,j] == 0:
                 a, b, c, d = computeValley(img, labelX, labelY, i, j, h, bw)
             elif img[i,j] == -2:
-                a,b,c,d = 0,0,0,0
-                img_out[i,j], equal = clostestSegment(img, labelX, labelY, i,j)
-                if equal:
-                    c, d = 0.5, 0.5
+                #a,b,c,d = 0,0,0,0
+                img_out[i,j], a, b, c, d = clostestSegment(img, labelX, labelY, i,j, h, bw)
+                # if equal:
+                #     c, d = 0.5, 0.5
             else:
                 a, b, c, d = computeSurface(img, labelX, labelY, i, j, h, bw)
             nx = -(2*a*i+c)
@@ -257,9 +280,9 @@ def visualize(img, outPath, colorCoding, minV, maxV, useCC = True, colormap = No
     if not useCC:
         colorCoding = img
     colorCoding = 1.0/(maxC-minC)*(colorCoding-minC)
-    img_c = cm.jet(colorCoding, bytes=True)
+    #img_c = cm.jet(colorCoding, bytes=True)
     # Some manual color coding
-    # img_c = np.zeros((*img.shape,4), dtype="uint8")
+    img_c = np.zeros((*img.shape,4), dtype="uint8")
     # img_c[colorCoding==1] = [27, 158, 119, 255]
     # img_c[colorCoding==4] = [217, 95, 2, 255]
     # img_c[colorCoding==3] = [117, 112, 179, 255]
@@ -269,8 +292,8 @@ def visualize(img, outPath, colorCoding, minV, maxV, useCC = True, colormap = No
         for s in colormap.keys():
             img_c[img==s] = 255*np.array(colormap[s])
     # Some other manual color map
-    #img_c[colorCoding == 0] = [255, 255, 255, 255]
-    #img_c[img != 0] = [255, 0, 0, 255]
+    img_c[colorCoding == 0] = [255, 255, 255, 255]
+    img_c[img != 0] = [255, 100, 100, 255]
     #img_c[img == 1] = [0.215686*255, 0.494118*255, 0.721569*255, 255]#[228, 26, 28, 255] # red
     #img_c[img == 2] = [0.596078*255, 0.305882*255, 0.639216*255, 255]#[55, 126, 184, 255] # blue
     #img_c[img == 3] = [0.894118*255, 0.101961*255, 0.109804*255, 255]#[152, 78, 163, 255] # purple
