100c19d8a2e849901a954817435e6b24cfdcc4fb
Marina Evers <marina.evers@uni-muenster.de>
2024-06-28 16:08:08 +0200

    Adapt for only showing segmentation

--- src/utils.py
+++ src/utils.py
@@ -4,4 +4,6 @@ import mapping.rendering as r
 
 
 def draw(data, path, filename, img = None, colormap=None):
+    if img == None:
+        img = data
     r.visualize(img, os.path.join(path, filename + ".png"), data, minV=np.min(data), maxV=np.max(data), useCC=True, colormap=colormap)
